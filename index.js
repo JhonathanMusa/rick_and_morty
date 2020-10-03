9a1d2e86340cfb72951bf4e2ec44a54bd4b139f5
jhonathanmusa <jonathansm891@gmail.com>
2020-10-03 18:45:34 -0500

    thirth commit

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const path = require("path")
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/views/index.html"))
-
 })
 
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"))
-
 })
 
-app.get("/contact-me", (req, res) => {
+app.get("/contact", (req, res) => {
     res.sendFile(path.join(__dirname + "/views/contact-me.html"))
-
 })
 
 app.listen(process.env.port || 8000);
