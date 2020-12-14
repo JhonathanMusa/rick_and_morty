01fac08af71e19f398ed20a64f53b4e03748d739
jonathanmusa <jonathansm891@gmail.com>
2020-12-13 21:33:10 -0500

    changing default port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
 // const path = require("path");
+const PORT = process.env.port || 3000
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(require("./routes/index"));
 
-app.listen(process.env.port || 8000);
-console.log("Server running on port 8000");
+app.listen(PORT);
+console.log("Server running on port " + PORT);
