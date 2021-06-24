919fd4e453cc613b5b0e117138ff179d0ba12c29
jhonathanmusa <jhonathansamu91@gmail.com>
2021-06-24 15:27:56 -0500

    improving card details

--- src/Components/DetailsCard.js
+++ src/Components/DetailsCard.js
@@ -1,12 +1,12 @@
-import styles from "./Details.module.css";
+import style from "./Details.module.css"
 
 export const DetailsCard = ({ details }) => {
   return (
-    <div className={styles.cardGrid}>
-      <img src={details.image} alt="" />
+    <div className={style.cardDetailsGrid}>
+      <img src={details.image} alt="" className={style.detailsImg} />
       <p>{details.name}</p>
       <p>{details.species}</p>
-      <p>{details.status}</p>
+      <p><i className="fas fa-circle"></i> {details.status}</p>
     </div>
   );
 };
