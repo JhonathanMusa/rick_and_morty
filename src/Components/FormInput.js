e0df26fcd4b723fc5cae3cf72f9dc1425d3ebce0
jhonathanmusa <jhonathansamu91@gmail.com>
2021-08-16 21:51:29 -0500

    updated formInput component

--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -2,14 +2,15 @@ import React from "react";
 import style from "./CharacterSearch.module.css";
 
 export const FormInput = (props) => {
+  const { name, handleChange, placeholder, type, value } = props;
   return (
     <input
       className={style.filter_input}
-      name={props.name}
-      onChange={props.handleChange}
-      placeholder={props.placeholder}
-      type={props.type}
-      value={props.value}
+      name={name}
+      onChange={handleChange}
+      placeholder={placeholder}
+      type={type}
+      value={value}
     />
   );
 };
